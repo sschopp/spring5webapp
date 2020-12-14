9f9fee3ce2762691662d17cb5ab6352421109da4
Sebastian Schopp <s.schopp@dasgip.de>
2020-12-14 17:21:14 +0100

    added repositories

--- src/main/java/guru/springframework/spring5webapp/repositories/AuthorRepository.java
+++ src/main/java/guru/springframework/spring5webapp/repositories/AuthorRepository.java
@@ -1,4 +1,10 @@
 package guru.springframework.spring5webapp.repositories;
 
-public class AuthorRepository {
+import guru.springframework.spring5webapp.domain.Author;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+
 }
